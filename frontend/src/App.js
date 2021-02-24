a1b3127603c396e638f3b5d50c661b8b212404e7
Navrisham <navrisham1@gmail.com>
2021-02-24 17:07:01 -0500

    Implemented Redux for SHOPIT Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from './components/Home'
+import ProductDetails from './components/product/ProductDetails'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header> </Header>
+        <div className="container container-fluid">
+        <Route path ="/" component={Home} exact />
+        <Route path ="/product/:id" component={ProductDetails} exact />
+        </div>
+        <Footer></Footer>
+        <h1></h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
